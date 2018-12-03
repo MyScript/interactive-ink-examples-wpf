4342a8f8c56de856eb7b07a3733844b5c32d1eb2
MyScript <packages@myscript.com>
2018-12-03 17:40:04 +0100

    Fix emojis and ligatures rendering

--- UIReferenceImplementation/FontMetricsProvider.cs
+++ UIReferenceImplementation/FontMetricsProvider.cs
@@ -36,13 +36,10 @@ namespace MyScript.IInk.UIReferenceImplementation
 
         private static void GetGlyphMetrics_(MyScript.IInk.Text.Text text, TextSpan[] spans, List<GlyphMetrics> glyphMetrics, float dpiX, float dpiY)
         {
-            var drawing = new DrawingGroup();
-            var ctx = drawing.Open();
-
-            float baseline = 0.0f;
-            int spanCount = 0;
-            foreach (var textSpan in spans)
+            // Process glyph one by one to generate one box per glyph
+            for (int s = 0; s < spans.Length; ++s)
             {
+                var textSpan = spans[s];
                 var fontFamily = new FontFamily(textSpan.Style.FontFamily);
                 var fontSize = mm2px(textSpan.Style.FontSize, dpiY);
                 var fontWeight = FontWeight.FromOpenTypeWeight(textSpan.Style.FontWeight);
@@ -63,27 +60,24 @@ namespace MyScript.IInk.UIReferenceImplementation
 
                 var typeFace = new Typeface(fontFamily, fontStyle, fontWeight, fontStretch);
 
-                // Process glyph one by one to generate one box per glyph
-                var label = "";
-
                 for (int j = textSpan.BeginPosition; j < textSpan.EndPosition; ++j)
                 {
                     var glyphLabel = text.GetGlyphLabelAt(j);
 
-                    var formattedText = new FormattedText
+                    var formattedChar = new FormattedText
                                         (
                                             glyphLabel, System.Globalization.CultureInfo.CurrentCulture,
                                             FlowDirection.LeftToRight, typeFace, fontSize, Brushes.Black
                                         );
 
-                    formattedText.TextAlignment = TextAlignment.Left;
+                    formattedChar.TextAlignment = TextAlignment.Left;
 
-                    var geometry = formattedText.BuildGeometry(new System.Windows.Point(0.0f, 0.0f));
+                    var geometry = formattedChar.BuildGeometry(new System.Windows.Point(0.0f, 0.0f));
                     var rect = geometry.Bounds;
 
                     // For glyph without geometry (space)
                     if (rect.IsEmpty)
-                        rect = new Rect(0.0, 0.0, formattedText.Width, formattedText.Height);
+                        rect = new Rect(0.0, 0.0, formattedChar.Width, formattedChar.Height);
                          
                     var rectX = (float)rect.X;
                     var rectY = (float)rect.Y;
@@ -102,97 +96,161 @@ namespace MyScript.IInk.UIReferenceImplementation
                     var glyphRightBearing = px2mm(rightBearing, dpiX);
 
                     glyphMetrics.Add(new GlyphMetrics(glyphRect, glyphLeftBearing, glyphRightBearing));
-
-                    label += glyphLabel;
                 }
-
-                // Draw current span
-                {
-                    var formattedText = new FormattedText
-                                        (
-                                            label, System.Globalization.CultureInfo.CurrentCulture,
-                                            FlowDirection.LeftToRight, typeFace, fontSize, Brushes.Black
-                                        );
-
-                    formattedText.TextAlignment = TextAlignment.Left;
-
-                    if (spanCount == 0)
-                        baseline = (float)formattedText.Baseline;
-
-                    ctx.DrawText(formattedText, new System.Windows.Point(0.0, 0.0));
-                }
-
-                ++spanCount;
             }
 
-            ctx.Close();
+            if (glyphMetrics.Count == 0)
+                return;
+
+            // Draw text to get data for glyphs
+            float baseline = 0.0f;
+            FormattedText formattedText;
+
+            {
+                var firstStyle = spans[0].Style;
+                var firstFontFamily = new FontFamily(firstStyle.FontFamily);
+                var firstFontSize = mm2px(firstStyle.FontSize, dpiY);
+                var firstFontWeight = FontWeight.FromOpenTypeWeight(firstStyle.FontWeight);
+                var firstFontStretch = FontStretches.Normal;
+                var firstFontStyle = FontStyles.Normal;
+
+                if (firstStyle.FontStyle.Equals("italic"))
+                    firstFontStyle =  FontStyles.Italic;
+                else if (firstStyle.FontStyle.Equals("oblique"))
+                    firstFontStyle =  FontStyles.Oblique;
+
+                if (firstStyle.FontWeight >= 700)
+                    firstFontWeight = FontWeights.Bold;
+                else if (firstStyle.FontWeight >= 400)
+                    firstFontWeight = FontWeights.Normal;
+                else
+                    firstFontWeight = FontWeights.Light;
+
+                var firstFontTypeFace = new Typeface(firstFontFamily, firstFontStyle, firstFontWeight, firstFontStretch);
+
+                formattedText = new FormattedText
+                                    (
+                                        text.Label, System.Globalization.CultureInfo.CurrentCulture,
+                                        FlowDirection.LeftToRight, firstFontTypeFace, firstFontSize,
+                                        Brushes.Black
+                                    );
+
+                formattedText.TextAlignment = TextAlignment.Left;
+
+                for (int s = 0; s < spans.Length; ++s)
+                {
+                    var textSpan = spans[s];
+                    var charIndex = textSpan.BeginPosition;
+                    var charCount = textSpan.EndPosition - textSpan.BeginPosition;
+
+                    var fontFamily = new FontFamily(textSpan.Style.FontFamily);
+                    var fontSize = mm2px(textSpan.Style.FontSize, dpiY);
+                    var fontWeight = FontWeight.FromOpenTypeWeight(textSpan.Style.FontWeight);
+                    var fontStretch = FontStretches.Normal;
+                    var fontStyle = FontStyles.Normal;
+
+                    if (textSpan.Style.FontStyle.Equals("italic"))
+                        fontStyle =  FontStyles.Italic;
+                    else if (textSpan.Style.FontStyle.Equals("oblique"))
+                        fontStyle =  FontStyles.Oblique;
+
+                    if (textSpan.Style.FontWeight >= 700)
+                        fontWeight = FontWeights.Bold;
+                    else if (textSpan.Style.FontWeight >= 400)
+                        fontWeight = FontWeights.Normal;
+                    else
+                        fontWeight = FontWeights.Light;
+
+                    var fontTypeFace = new Typeface(fontFamily, fontStyle, fontWeight, fontStretch);
+
+                    formattedText.SetFontFamily(fontFamily, charIndex, charCount);
+                    formattedText.SetFontSize(fontSize, charIndex, charCount);
+                    formattedText.SetFontWeight(fontWeight, charIndex, charCount);
+                    formattedText.SetFontStretch(fontStretch, charIndex, charCount);
+                    formattedText.SetFontStyle(fontStyle, charIndex, charCount);
+                    formattedText.SetFontTypeface(fontTypeFace, charIndex, charCount);
+                }
+
+                baseline = (float)formattedText.Baseline;
+            }
+
+            var drawing = new DrawingGroup();
+            {
+                var ctx = drawing.Open();
+                ctx.DrawText(formattedText, new System.Windows.Point(0.0, 0.0));
+                ctx.Close();
+            }
 
             // Apply baseline and offsets of glyphs to bounding boxes
-            if (glyphMetrics.Count > 0)
-                WalkDrawingForText(drawing, glyphMetrics, baseline, dpiX, dpiY);
+            WalkDrawingForText(drawing, glyphMetrics, baseline, dpiX, dpiY);
         }
 
         private static void WalkDrawingForText_(Drawing drawing, List<GlyphMetrics> glyphMetrics, ref int idx, ref float x, float baseline, float dpiX, float dpiY)
         {
-            var glyphs = drawing as GlyphRunDrawing;
+            // Parse the Drawing tree recursively depending on the real type of each node
+            // - node is a DrawingGroup, parse the children
+            // - node is a GlyphRunDrawing, retrieve the glyphs data
 
             if (idx >= glyphMetrics.Count)
                 return;
 
+            var glyphs = drawing as GlyphRunDrawing;
+
             if (glyphs != null)
             {
                 var glyphRun = glyphs.GlyphRun;
-                var glyphCount = glyphRun.AdvanceWidths.Count;
-                var charCount = glyphRun.ClusterMap != null ? glyphRun.ClusterMap.Count : glyphCount;
 
-                for (int g = 0; g < charCount; ++g)
+                if (    (glyphRun.Characters != null) && (glyphRun.ClusterMap != null)
+                    &&  (glyphRun.Characters.Count == glyphRun.ClusterMap.Count) )
                 {
-                    if (idx >= glyphMetrics.Count)
-                        break;
+                    var text = new String(glyphRun.Characters.ToArray());
+                    var tee = System.Globalization.StringInfo.GetTextElementEnumerator(text);
 
-                    if ( (g > 0) && glyphRun.ClusterMap != null && (glyphRun.ClusterMap[g] == glyphRun.ClusterMap[g-1]) )
+                    while (tee.MoveNext())
                     {
-                        // Current character shares its glyph with the previous character (ligature)
+                        if (idx >= glyphMetrics.Count)
+                            break;
 
-                    #if false
-                        // Recompute box of previous glyph
-                        var i = glyphRun.GlyphIndices[glyphRun.ClusterMap[g-1]];
+                        var g = tee.ElementIndex;
 
-                        var rectX = (float)(glyphRun.GlyphTypeface.LeftSideBearings[i] * glyphRun.FontRenderingEmSize);
-                        var rectY = (float)(glyphRun.GlyphTypeface.BottomSideBearings[i] * glyphRun.FontRenderingEmSize);
-                        var rectW = (float)((glyphRun.GlyphTypeface.AdvanceWidths[i] - glyphRun.GlyphTypeface.LeftSideBearings[i] - glyphRun.GlyphTypeface.RightSideBearings[i]) * glyphRun.FontRenderingEmSize);
-                        var rectH = (float)((glyphRun.GlyphTypeface.AdvanceHeights[i] - glyphRun.GlyphTypeface.TopSideBearings[i] - glyphRun.GlyphTypeface.BottomSideBearings[i]) * glyphRun.FontRenderingEmSize);
+                        if ( (g > 0) && (idx > 0) && (glyphRun.ClusterMap[g] == glyphRun.ClusterMap[g-1]) )
+                        {
+                            // Ligature with the previous glyph
+                            // The position is not accurate because of glyphs substitution at rendering
+                            // but it makes the illusion.
+                            var prevGlyphMetrics = glyphMetrics[idx-1];
+                            glyphMetrics[idx].BoundingBox.Y -= px2mm(baseline, dpiY);
+                            glyphMetrics[idx].BoundingBox.X = prevGlyphMetrics.BoundingBox.X
+                                                            + prevGlyphMetrics.BoundingBox.Width
+                                                            + prevGlyphMetrics.RightSideBearing
+                                                            + glyphMetrics[idx].LeftSideBearing;
+                        }
+                        else
+                        {
+                            glyphMetrics[idx].BoundingBox.Y -= px2mm(baseline, dpiY);
+                            glyphMetrics[idx].BoundingBox.X += px2mm(x, dpiX);
+                            x += (float)glyphRun.AdvanceWidths[glyphRun.ClusterMap[g]];
+                        }
 
-                        var leftBearing = -(float)(rectX);
-                        var rightBearing = 0.0f;
-
-                        var glyphX = px2mm(rectX, dpiX);
-                        var glyphY = px2mm(rectY, dpiY);
-                        var glyphW = px2mm(rectW, dpiX);
-                        var glyphH = px2mm(rectH, dpiY);
-                        var glyphRect = new Rectangle(glyphX, glyphY, glyphW, glyphH);
-                        var glyphLeftBearing = px2mm(leftBearing, dpiX);
-                        var glyphRightBearing = px2mm(rightBearing, dpiX);
-
-                        x -= (float)glyphRun.AdvanceWidths[glyphRun.ClusterMap[g-1]];
-                        glyphMetrics[idx-1] = new GlyphMetrics(glyphRect, glyphLeftBearing, glyphRightBearing);
-                        glyphMetrics[idx-1].BoundingBox.Y -= px2mm(baseline, dpiY);
-                        glyphMetrics[idx-1].BoundingBox.X += px2mm(x, dpiX);
-                        x += (float)glyphRun.AdvanceWidths[glyphRun.ClusterMap[g-1]];
-                    #endif
-
-                        // => Use previous box for current character
-                        glyphMetrics[idx] = glyphMetrics[idx-1];
-
+                        ++idx;
                     }
-                    else
+                }
+                else
+                {
+                    // Fallback in case we have no data for characters and clusters
+                    var glyphCount = glyphRun.AdvanceWidths.Count;
+
+                    for (int g = 0; g < glyphCount; ++g)
                     {
+                        if (idx >= glyphMetrics.Count)
+                            break;
+
                         glyphMetrics[idx].BoundingBox.Y -= px2mm(baseline, dpiY);
                         glyphMetrics[idx].BoundingBox.X += px2mm(x, dpiX);
-                        x += (float)glyphRun.AdvanceWidths[glyphRun.ClusterMap != null ? glyphRun.ClusterMap[g] : g];
-                    }
 
-                    ++idx;
+                        x += (float)glyphRun.AdvanceWidths[g];
+                        ++idx;
+                    }
                 }
             }
             else
