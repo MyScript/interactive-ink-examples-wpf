d40262e62f24fe484b7d32390f3ea54f89d9511b
MyScript <packages@myscript.com>
2020-06-11 20:18:05 +0200

    Update MyScript.InteractiveInk.Wpf to version 1.4.0

--- GetStarted/Properties/AssemblyInfo.cs
+++ GetStarted/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MyScript")]
 [assembly: AssemblyProduct("MyScript.InteractiveInk.GetStarted.Wpf")]
-[assembly: AssemblyCopyright("Copyright ©  2017, 2018, 2019")]
+[assembly: AssemblyCopyright("Copyright MyScript. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
